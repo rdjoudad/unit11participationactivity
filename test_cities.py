a92448842052a47f3fe3e2ef81f2b657b57c9f1c
Ryma Djoudad <zenyod85@gmail.com>
2026-04-05 17:14:50 -0400

    11-2 (intentional fail)

--- test_cities.py
+++ test_cities.py
@@ -1,4 +1,4 @@
 from city_functions import country_capital
 
 def test_city_country():
-    assert country_capital("Santiago", "Chile") == "Santiago, Chile"
+    assert country_capital("Santiago", "Chile", population=5000000) == "Santiago, Chile, 5000000"
